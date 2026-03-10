c9b54e99ae867a405add0f2e4856bff27f752fea
Yanchev <yanchev.mu@gmail.com>
2026-03-10 19:10:50 +0200

    updated tasks for contacts.py and notes.py

--- contacts.py
+++ contacts.py
@@ -2,10 +2,16 @@ class Contacts:
     def add_contact(self, name: str, email: str):
         pass
 
-    def remove_contact(self, name: str):
+    # public methods will be nothing returning
+    # and will get arguments like command string words
+    # and will call the private methods
+    # method print message for user to console
+    # method raise extensions 
+    # if arguments don't match the expected format    
+    def remove_contact(self, *args):
         pass
 
-    def get_contact(self, name: str) -> str:
+    def get_contact(self, *args) -> str:
         pass
 
     # and any other methods you think are necessary 
--- notes.py
+++ notes.py
@@ -4,24 +4,34 @@ class Notes:
     def __init__(self):
         self.notes = []
 
-    def add_note(self, note: str):
+    # will make all the methods private
+    def __add_note(self, note: str):
         """Add a new note."""
         self.notes.append(note)
         return "Note added."
 
-    def get_notes(self) -> list[str]:
+    def __get_notes(self) -> list[str]:
         return self.notes
 
-    def delete_note(self, index: int):
+    def __delete_note(self, index: int):
         if 0 <= index < len(self.notes):
             return self.notes.pop(index)
         return None
 
-    def edit_note(self, index: int, new_note: str):
+    def __edit_note(self, index: int, new_note: str):
         if 0 <= index < len(self.notes):
             self.notes[index] = new_note
             return True
         return False
 
-    def search_notes(self, keyword: str) -> list[str]:
+    def __search_notes(self, keyword: str) -> list[str]:
         return [note for note in self.notes if keyword.lower() in note.lower()]
+    
+    # public methods will be nothing returning
+    # and will get arguments like command string words
+    # and will call the private methods
+    # method print message for user to console
+    # method raise extensions 
+    # if arguments don't match the expected format
+    def add_note(self, *args):
+        pass
